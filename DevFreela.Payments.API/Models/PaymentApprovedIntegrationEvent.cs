bccec3fa5d4d3b7a4c475dd6d2bc09ce95722a93
adrianapereira27 <pereira.adri@gmail.com>
2024-03-22 17:47:29 -0300

    Criação de um fluxo de mensageria com RabbitMQ

---
+++ DevFreela.Payments.API/Models/PaymentApprovedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.Payments.API.Models
+{
+    public class PaymentApprovedIntegrationEvent
+    {
+        public PaymentApprovedIntegrationEvent(int idProjeto)
+        {
+            IdProjeto = idProjeto;
+        }
+
+        public int IdProjeto { get; set;}
+    }
+}
--- DevFreela.Payments.API/Program.cs
+++ DevFreela.Payments.API/Program.cs
@@ -1,3 +1,4 @@
+using DevFreela.Payments.API.Consumers;
 using DevFreela.Payments.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddHostedService<ProcessPaymentConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
